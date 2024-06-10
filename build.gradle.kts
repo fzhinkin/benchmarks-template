d40fb2e725f3da74041220fce0be4efdf1b60b43
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2024-06-10 14:42:54 +0200

    Update Kotlin, kx-benchmark and NodeJs(implicitly)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,9 @@
 import kotlinx.benchmark.gradle.JvmBenchmarkTarget
 import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
-import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
-import org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinNpmInstallTask
 
 plugins {
-    kotlin("multiplatform") version "1.9.22"
-    id("org.jetbrains.kotlinx.benchmark") version "0.4.10"
+    kotlin("multiplatform") version "2.0.0"
+    id("org.jetbrains.kotlinx.benchmark") version "0.4.11"
 }
 
 group = "org.example"
@@ -31,7 +29,7 @@ kotlin {
     sourceSets {
         commonMain {
             dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-benchmark-runtime:0.4.10")
+                implementation("org.jetbrains.kotlinx:kotlinx-benchmark-runtime:0.4.11")
             }
         }
     }
@@ -55,13 +53,4 @@ benchmark {
             advanced("jvmForks", 3)
         }
     }
-}
-
-rootProject.the<NodeJsRootExtension>().apply {
-    nodeVersion = "21.0.0-v8-canary202310177990572111"
-    nodeDownloadBaseUrl = "https://nodejs.org/download/v8-canary"
-}
-
-rootProject.tasks.withType<KotlinNpmInstallTask>().configureEach {
-    args.add("--ignore-engines")
 }
