912db80b4c052f0b852c1c413f7e86b40916caf5
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2024-08-30 09:34:59 -0400

    Update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import kotlinx.benchmark.gradle.JvmBenchmarkTarget
 import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 
 plugins {
-    kotlin("multiplatform") version "2.0.0"
-    id("org.jetbrains.kotlinx.benchmark") version "0.4.11"
+    kotlin("multiplatform") version "2.0.20"
+    id("org.jetbrains.kotlinx.benchmark") version "0.4.12"
 }
 
 group = "org.example"
@@ -29,7 +29,7 @@ kotlin {
     sourceSets {
         commonMain {
             dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-benchmark-runtime:0.4.11")
+                implementation("org.jetbrains.kotlinx:kotlinx-benchmark-runtime:0.4.12")
             }
         }
     }
